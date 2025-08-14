ae67c6c6fd93da3c51f5c61d90bbedd871aadf70
Tiaan Dorfling <purnjappie@gmail.com>
2025-08-14 17:48:10 +0200

    Basic menu with option to calc and exit.

--- Linear Programming/Program.cs
+++ Linear Programming/Program.cs
@@ -14,17 +14,41 @@ namespace Linear_Programming
             Console.Read();
         }
 
-        Enum menu
+        enum menu
         {
             Calculate,
+            Exit
         }
 
         static void DisplayMenu()
         {
-            Console.WriteLine("Welcome to Linear Pramming Calculator\n\n");
-            foreach (var var in menu.GetValues(typeof(Enum)))
+            Console.WriteLine("Welcome to Linear Pramming Calculator\n");
+            foreach (var var in Enum.GetValues(typeof(menu)))
             {
-                Console.WriteLine($"{var.ToString()}");
+                Console.WriteLine($"{(int)var + 1}. {var.ToString()}");
+            }
+            GetMenuInput();
+        }
+
+        static void GetMenuInput()
+        {
+            Console.Write("\nEnter your option: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Time to Calculate!");
+                    break;
+                case 2:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Try again.");
+                    Console.Clear();
+                    DisplayMenu();
+                    break;
             }
         }
 
